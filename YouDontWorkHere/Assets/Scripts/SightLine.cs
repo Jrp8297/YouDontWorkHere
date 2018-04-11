454fcbb33bdc157e46131f82cc5f7b5847ac4109
MAIN\jrp8297 <jrp8297@g.rit.edu>
2018-04-11 10:56:15 -0400

    Cannbilized Code from "DTRH"

---
+++ YouDontWorkHere/Assets/Scripts/SightLine.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class SightLine : MonoBehaviour
+{
+    Vector3 position;
+    Vector3 playerPosition;
+    Vector3 offset;
+    Vector3 leftPos, rightPos, centerPos;
+
+    float minAng, maxAng;
+
+    LineRenderer myRenderer;
+
+   
+    public GameObject player;
+    public float offsetAngle;
+    public float widthAngle;
+    public float Radius;
+    public Material mat;
+    public bool doRaycast;
+    public LayerMask raycastingMask;
+
+    static private bool isPaused = false;
+    static public bool IsPaused
+    {
+        get { return isPaused; }
+        set { isPaused = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        isPaused = false;
+        if (!player)
+        {
+            player = FindObjectOfType<Player>().gameObject;
+        }
+        myRenderer = GetComponent<LineRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused) return;
+
+        position = gameObject.transform.position;
+        playerPosition = player.transform.position;
+        // Vector3 playerToMonster = playerPosition - position;
+        // playerPosition = playerPosition - (playerToMonster.normalized * .35f);
+        offset = playerPosition - position;
+        offsetAngle = gameObject.transform.rotation.eulerAngles.z;
+
+
+
+
+        //Draws a sight line
+        maxAng = 90 + offsetAngle + (widthAngle / 2);//Far angle           
+        minAng = 90 + (offsetAngle - (widthAngle / 2));
+
+        float maxAngRad = maxAng * Mathf.Deg2Rad;
+        float minAngRad = minAng * Mathf.Deg2Rad;
+
+
+        leftPos.x = position.x + Radius * (Mathf.Cos(maxAngRad));
+        leftPos.y = position.y + Radius * (Mathf.Sin(maxAngRad));
+        leftPos.z = -.1f;
+
+        centerPos.x = position.x + Radius * (Mathf.Cos((90 + offsetAngle) * Mathf.Deg2Rad));
+        centerPos.y = position.y + Radius * (Mathf.Sin((90 + offsetAngle) * Mathf.Deg2Rad));
+        centerPos.z = 0.0f;
+
+        rightPos.x = position.x + Radius * (Mathf.Cos(minAngRad));
+        rightPos.y = position.y + Radius * (Mathf.Sin(minAngRad));
+        rightPos.z = -.1f;
+        position.z = -.1f;
+
+        myRenderer.SetPosition(0, position);
+        myRenderer.SetPosition(1, leftPos);
+        myRenderer.SetPosition(2, centerPos);
+        myRenderer.SetPosition(3, rightPos);
+
+
+        if (offset.sqrMagnitude < Radius * Radius)//Circle Collision Check;
+        {
+            if (SightLineCheck())
+            {
+                Debug.Log("HIT");               
+                isPaused = true;
+            }
+
+        }
+
+    }
+
+    bool SightLineCheck()
+    {
+        if (widthAngle >= 360)//If this is a circle, just return true, since this is calld after a circle collision check
+        {
+            return true;
+        }
+        else
+        {
+            float rad = Mathf.Atan2(offset.y, offset.x);//Get the angle that the player is from the sightline's source.
+            float playerAng = rad * 180 / Mathf.PI; // convert to angles          
+
+
+            if (playerAng < 0)
+            {
+                playerAng += 360;
+            }
+            //Debug.Log( "Player angle is:" + playerAng + " Range of hit angles is:" + minAng + " to " + maxAng +" ,offset is :"+ offset.sqrMagnitude);
+
+            if (minAng > 360)
+            {
+                minAng -= 360;
+            }
+
+            if (maxAng > 360)//if we are looping around 0
+            {
+                maxAng -= 360;//set the Maximum angle to a small number(below 360).                
+
+            }
+
+
+            if (minAng > maxAng)//if we are looping around 0
+            {
+                if (playerAng > minAng || playerAng < maxAng)
+                {//This is OR not AND because of the looping around 0;
+                    if (doRaycast)
+                    {
+
+                    }
+                    return true;
+                }
+                return false;
+
+            }
+            else//If we aren't looping around 0
+            {//If the player is Greater than the minimum and less than the maximum
+                if (playerAng > minAng && playerAng < maxAng)
+                {
+                    if (doRaycast)
+                    {
+                        RaycastHit2D didHit = Physics2D.Raycast(position, offset, Radius * 2, raycastingMask);
+                        if (didHit.collider.tag == "Player")
+                        {
+                            return true;
+                        }
+                        return false;
+
+                    }
+                    return true;
+                }
+                return false;
+            }
+
+        }
+
+
+    }
+
+
+}
