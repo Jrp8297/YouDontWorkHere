0c9c6161e54cacd065e78160d908783734f473c5
Joel <Jrp8297@g.rit.edu>
2018-04-12 23:20:02 -0400

    Added basic table and consumer functionality

---
+++ YouDontWorkHere/Assets/Scripts/ConsumerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsumerScript : MonoBehaviour {
+    public float phaseTimer;
+    public int phase =0;
+    SpriteRenderer myRenderer;
+    TableScript myTable;
+    Color mood;
+
+
+	// Use this for initialization
+	void Start () {
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
+        myTable = gameObject.GetComponentInParent<TableScript>();
+        mood.a = 1;
+        mood.r = 1;
+        phaseTimer = 0;
+        phase = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        phaseTimer += Time.deltaTime;
+        if(phaseTimer > 4)
+        {
+            phaseTimer = 0;
+            phase += 1;
+        }
+
+
+        if (phase == 0)
+        {
+            mood.b = (1.0f - (phaseTimer / 4.0f));
+            mood.g = (1.0f - (phaseTimer / 4.0f));
+
+            myRenderer.color = mood;
+        }
+        if (phase == 1)
+        {
+            mood.b = 1;
+            mood.r = (1.0f - (phaseTimer / 4.0f));
+            mood.g = (1.0f - (phaseTimer / 4.0f));
+
+            myRenderer.color = mood;
+        }
+        if (phase == 2)
+        {
+            mood.b = (1.0f - (phaseTimer / 4.0f));
+            mood.r = (1.0f - (phaseTimer / 4.0f));
+            mood.g = 1;
+
+            myRenderer.color = mood;
+        }
+        if (phase >= 3)
+        {
+            myTable.spawned = false;
+            Destroy(gameObject);
+        }
+
+
+    }
+}
---
+++ YouDontWorkHere/Assets/Scripts/TableScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableScript : MonoBehaviour {
+    public GameObject ConsumerPrefab;
+    public float timerMax;
+    public float timerCur;
+    public bool spawned = false;    
+    private Quaternion baseQaut;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!spawned){//tick the clock if we can spawn stuff
+
+            timerCur += Time.deltaTime;//tick clock
+
+            if (timerCur > timerMax)
+            {//compare to max value
+                Vector3 tempPos = gameObject.transform.position;
+                tempPos.y += 4;
+                Instantiate(ConsumerPrefab, tempPos, baseQaut, gameObject.transform);//create a consumer above the table
+                timerCur = 0;
+                spawned = true;
+            }
+        }
+	}
+}
