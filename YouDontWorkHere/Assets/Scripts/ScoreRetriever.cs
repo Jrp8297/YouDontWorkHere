0fdf53ec7b680655b597a94e7004ca6737297769
Joel Petritsch (RIT Student) <jrp8297@ad.rit.edu>
2018-05-06 16:22:24 -0400

    Score is displayed at end of game

---
+++ YouDontWorkHere/Assets/Scripts/ScoreRetriever.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreRetriever : MonoBehaviour {
+
+    public Text toDisplay;
+    // Use this for initialization
+    void Start () {
+        toDisplay = gameObject.GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        toDisplay.text = PlayerPrefs.GetInt("playerScore").ToString();
+    }
+}
